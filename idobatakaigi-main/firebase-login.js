06c03f5552fd12e6ebf7eb545e5705e2767c8829
Takayuki Honda <takka925H@gmail.com>
2021-08-11 00:48:07 +0900

    error not solve

---
+++ idobatakaigi-main/firebase-login.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+const firebaseConfig = {
+    // ...
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
